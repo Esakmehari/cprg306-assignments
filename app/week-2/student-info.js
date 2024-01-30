058a79334694476147851cfd5bd517ebee31e94b
Esakmehari <131406362+Esakmehari@users.noreply.github.com>
2024-01-29 21:19:42 -0700

    Initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+export default function StudentInfo() {
+    return (
+      <div>
+        <p>Name: Esak Mehari</p>
+        <p>
+          GitHub Repository: <Link href="https://github.com/Esakmehari/cprg306-assignments">Esak's GitHub</Link>
+        </p>
+      </div>
+    );
+  }
