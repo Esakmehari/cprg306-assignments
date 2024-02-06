1676183a3f9239cf1df406ea388fbe22bde62001
Esakmehari <131406362+Esakmehari@users.noreply.github.com>
2024-02-05 18:09:55 -0700

    This is the addition of my week-3assignment

---
+++ app/week-3/item.js
@@ -0,0 +1,10 @@
+
+function Item({ name, quantity, category }) {
+    return (
+      <li className="border p-4 my-3">
+        <strong>{name}</strong> <br/> buy {quantity} in {category}
+      </li>
+    );
+  }
+  
+  export default Item;
---
+++ app/week-3/page.js
@@ -0,0 +1,12 @@
+import ItemList from './item-list'; // Import other components as needed
+
+function Page() {
+  return (
+    <main className="text-2xl bg-black-100 min-h-screen p-4">
+      <h1 className="text-3xl font-semibold mb-4">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+}
+
+export default Page;
