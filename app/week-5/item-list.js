a2657b041cb83552181497961650fb2ed2f1ba6a
Esakmehari <131406362+Esakmehari@users.noreply.github.com>
2024-02-26 18:49:17 -0700

    Complete Week 5

---
+++ app/week-5/item-list.js
@@ -0,0 +1,42 @@
+"use client";
+import { useState } from 'react';
+import Item from './item.js';
+import items from './items.json';
+
+const ItemList = () => {
+  const [sortBy, setSortBy] = useState('name');
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === 'name') {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === 'category') {
+      return a.category.localeCompare(b.category);
+    }
+  });
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => setSortBy('name')}
+          style={{ backgroundColor: sortBy === 'name' ? 'lightblue' : 'white' }}
+        >
+          Sort by Name
+        </button>
+        <button
+          onClick={() => setSortBy('category')}
+          style={{ backgroundColor: sortBy === 'category' ? 'lightblue' : 'white' }}
+        >
+          Sort by Category
+        </button>
+      </div>
+      <ul>
+        {sortedItems.map(item => (
+          <Item key={item.id} {...item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemList;
