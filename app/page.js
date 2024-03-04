f0a384f7e8f9b4d72ca840059552b85dc5546e82
Esakmehari <131406362+Esakmehari@users.noreply.github.com>
2024-03-04 16:21:10 -0700

    week 6 update

--- app/page.js
+++ app/page.js
@@ -7,7 +7,8 @@ export default function Page() {
         <Link href="/week-2">Week 2 Assignment</Link><br/>
         <Link href="/week-3">Week 3 Assignment</Link><br/>
         <Link href="/week-4">week 4 Assignment</Link><br/>
-        <Link href="/week-5">week 5 Assignment</Link>
+        <Link href="/week-5">week 5 Assignment</Link><br/>
+        <Link href="/week-6">week 6 Assignment</Link>
 
         
       </p>
